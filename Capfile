bb1073613138cd1a3d47a9f269f4c95b3c85f604
ta07ki <60640482+takimotokyousuke@users.noreply.github.com>
2020-02-16 11:18:05 +0900

    master

--- Capfile
+++ Capfile
@@ -1,6 +1,5 @@
 # Load DSL and set up stages
 require "capistrano/setup"
-
 # Include default deployment tasks
 require "capistrano/deploy"
 require 'capistrano/rbenv'
--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '~> 3.1'
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:takimotokyousuke/chatspace.git'
+set :repo_url,  'git@github.com:takimotokyousuke/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/chatspace.pem'] 
+                  keys: ['~/.ssh/chat-space.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
